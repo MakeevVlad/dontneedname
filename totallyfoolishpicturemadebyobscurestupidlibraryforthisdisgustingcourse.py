5b463713a3b4aec98d6c3cee83f0fe80e1c3a3cc
avmmal <32042102+MakeevVlad@users.noreply.github.com>
2019-10-01 12:34:44 +0300

    Add files via upload

--- totallyfoolishpicturemadebyobscurestupidlibraryforthisdisgustingcourse.py
+++ totallyfoolishpicturemadebyobscurestupidlibraryforthisdisgustingcourse.py
@@ -1,9 +1,19 @@
 from graph import *
 import math
+import random as rm
 
+
+#Window settings
+windowSize(500, 500)
+canvasSize(500, 500)
+penSize(0)
+
+#sets compleat color of your pen
 def color(x):
     penColor(x)
     brushColor(x)
+
+#Creates sectors
 def sectors(x,y,phi,r, bg, fg):
     color(fg)
     circle(x,y,r)
@@ -17,36 +27,63 @@ def sectors(x,y,phi,r, bg, fg):
         if not len(phi)==i+1 and chi>phi[i+1]:
             chi=phi[i+2]
             i=i+2
-def dichotomy(x,y,N,alpha,l,q,delta):
+
+#Array that keeps tree to make it deletable
+#obj = []
+
+#Tree-fractal function
+def dichotomy(x,y,N, N0, alpha,l,q,delta, obj):
     if N==0:
         return
-    line(x,y,x+l*math.cos(alpha),y+l*math.sin(alpha))
-    dichotomy(x+l*math.cos(alpha),y+l*math.sin(alpha),N-1,alpha+delta,q*l,q,delta)
-    dichotomy(x+l*math.cos(alpha),y+l*math.sin(alpha),N-1,alpha-delta,q*l,q,delta)
-windowSize(10000,10000)
-canvasSize(10000,10000)
-penSize(0)
-color("white")
-color("yellow")
-rectangle(0,0,10000,900)
-alpha=[math.pi]
-while alpha[-1]<2*math.pi:
-    alpha.append(alpha[-1]+math.pi/200)
-#del alpha[-1] #needed if n=20
-sectors(1000,900,alpha,1000,"yellow","red")
-color("yellow")
-circle(1000,900,630)
-color("red")
-circle(1000,900,600)
-color("purple")
-penSize(12)
-dichotomy(1000,900,15,-math.pi/2,300,0.6,math.pi*0.3)
-dichotomy(100,900,15,-math.pi/2,100,0.8,math.pi*0.05)
-color("green")
-rectangle(0,900,10000,10000)
-color("cyan")
-dichotomy(300,920,15,-math.pi/2+math.pi/15,200,0.7,math.pi*0.1)
-color("orange")
-dichotomy(1500,1000,19,-math.pi/2-math.pi/10,100,0.8,math.pi*0.2)
+    if N >= N0 - 3:
+        color("brown")
+    else:
+        color("green")
+    penSize(N)
+    rnd_l = rm.uniform(0.97, 1.04)
+    rnd_a = rm.uniform(0.95 +0.04, 1.05 - 0.04)
+    alpha*=rnd_a
+    rnd_d = rm.uniform(0.95 +0.05, 1.05 - 0.05)
+    delta*=rnd_d
+    obj.append(line(x,y,x+l*math.cos(alpha),y+l*math.sin(alpha)))
+    dichotomy(x+l*math.cos(alpha),y+l*math.sin(alpha),N-1, N0, alpha+delta,q*l*rnd_l,q,delta, obj)
+    dichotomy(x+l*math.cos(alpha),y+l*math.sin(alpha),N-1, N0, alpha-delta,q*l*rnd_l,q,delta, obj)
+    dichotomy(x+l*math.cos(alpha),y+l*math.sin(alpha),N-1, N0, alpha,q*l*rnd_l,q,delta, obj)
 
+
+#Draws a tree
+def tree(x, y, l, N, q, obj):
+
+    for i in obj:
+        deleteObject(i)
+    color("green")
+    dichotomy(x, y ,N, N, -math.pi/2, l ,q,math.pi*0.2, obj)
+
+def main():
+    color("yellow")
+    rectangle(0,0,500, 500)
+
+
+    alpha=[math.pi]
+    while alpha[-1]<2*math.pi:
+        alpha.append(alpha[-1]+math.pi/50)
+
+    #Sun
+    sectors(250,250,alpha, 400,"yellow","red")
+    color("yellow")
+    circle(250, 250, 60)
+    color("red")
+    circle(250,250,40)
+
+    color("#FC7C0C")
+    rectangle(0, 500, 500, 250)
+    obj1 = []
+    obj2 = []
+    obj3 = []
+    tree(500*(0.6), 500*(0.5 + 1/5), 45, 7, 0.7, obj2)
+    tree(500*(0.3), 500*(0.5 + 1/3), 50, 7, 0.8, obj1)
+    tree(500*(0.8), 500*(0.5 + 0.4), 46, 7, 0.75, obj3)
+
+
+onTimer(main, 1)
 run()
